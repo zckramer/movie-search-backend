40194df9f6e82de4e1ad57d2bb7f3b10a90a5f84
Zachary Kramer <zckramer83@gmail.com>
2020-11-15 18:02:31 -0500

    got dummy data to display as a list

--- routes/film.js
+++ routes/film.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const BASE_URL = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/"
+const dummyData = [{movieTitle:"Beetlejuice"},{movieTitle: "Snow White"}]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('film', { data : res , title : "Hell"});
 });
+
+router.get('/:search', function(req, res, next) {
+  // search = document.getElementById('searchField').value
+  // fetch(BASE_URL + search, ()=> {
+  //   console.log(res)
+  // })
+  // res.send('searching for ' + req.params.search)
+
+  // dummy Test
+  res.render('film', { searchResults : dummyData })
+})
+
+
 
 module.exports = router;
