5c6a56864383a519fd70da0269f97db5c7261ddd
Zachary Kramer <zckramer83@gmail.com>
2020-11-15 20:48:00 -0500

    hit IMDB API and rendered Title and Poster

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var filmRouter = require('./routes/film');
-var testAPIrouter = require('./routes/testAPI');
 
 var app = express();
 
@@ -22,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/film', filmRouter);
-app.use('/test', testAPIrouter)
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
