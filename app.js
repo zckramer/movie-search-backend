1716be5345abec1a157abba9400549cef08d3f59
Zachary Kramer <zckramer83@gmail.com>
2020-11-22 16:14:51 -0500

    cleaned up new route, finished with basics, need to add DB upvote and downvote

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+
+
+var app = express();
+app.use(cors());
 
 var indexRouter = require('./routes/index');
 var filmRouter = require('./routes/film');
-
-var app = express();
+var searchRouter = require('./routes/search');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/film', filmRouter);
+app.use('/search', searchRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/search.js
+++ routes/search.js
@@ -3,16 +3,11 @@ var router = express.Router();
 
 const fetch = require('node-fetch');
 
-const BASE_URL = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/"
-
-// Default search page
-router.get('/', function(req, res, next) {
-  res.render('film', { searchResults : {} , title : "Movie Search 3k"});
-});
+const BASE_URL = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/"
 
 // Search route
 router.get('/:search', function(req, res, next) {
-  console.log("search params = ", req.params.film)
+  console.log("search params = ", req.params.search)
   fetch(BASE_URL + req.params.search, 
     {headers: {
       "x-rapidapi-key": "712cf7e599mshd8ec9264a3d789ep1033c0jsn37f3b002f14f",
