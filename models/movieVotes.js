54837e68d34726828b6796bd626ffb8edc1489be
Zachary Kramer <zckramer83@gmail.com>
2020-12-03 19:04:26 -0500

    added POST route to DB, now increment up down votes

---
+++ models/movieVotes.js
@@ -0,0 +1,9 @@
+const movieVotesModel = {
+    "id": "",
+    "title": "",
+    "voteData": {
+        "upvote": 0,
+        "downvote": 0
+    }
+}
+module.exports = movieVotesModel;
--- routes/db.js
+++ routes/db.js
@@ -1,14 +1,16 @@
 var express = require('express');
+var movieVotesModel = require('../models/movieVotes');
+
 var router = express.Router();
-var mongoose = require('mongoose');
-var assert = require('assert');
+
+const MongoClient = require('mongodb').MongoClient;
+
 
 const url = "mongodb+srv://zckramer:spicegirls@personalprojectcluster.6nak7.mongodb.net/ZKDB?retryWrites=true&w=majority"
 
 // GET db data
 router.get('/:movieID', async function(req, res, next) {
   let data;
-  const MongoClient = require('mongodb').MongoClient;
   const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
   client.connect( async err => {
     const collection = client.db("ZKDB").collection("MovieSearch");
@@ -16,57 +18,25 @@ router.get('/:movieID', async function(req, res, next) {
 
     if (cursor != null){
       data = cursor
-      console.log("Return Data: ", data) // this is logging what I want correctly
+      console.log("Return Data: ", data) 
       res.send(data);
     } else {
-      res.send({"asdf":"error"});
+      res.send(movieVotesModel);
     }
 
   })
   client.close()
 })
 
+// INSERT data (insertOne)
+router.post('/insert', function(req, res, next) {
+  const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect( async err => {
+    const collection = client.db("ZKDB").collection("MovieSearch");
+    await collection.insertOne(req.body);
+  })
+  res.status(201).send('Success!');
+  client.close()
+});
 
-  //     let data;
-//   await mongoose.connect(url, function(err, db) {
-//     assert.strictEqual(null, err);
-//     data = db.collection('MovieSearch').findOne({'id': req.params.movieID})
-//   })
-//   .then(db.close)
-//   return data
-// })
-
-  // var resultArray = [];
-  // mongo.connect(url, function(err, db) {
-  //   assert.strictEqual(null, err);
-  //     var cursor = db.collection('MovieSearch').find('{}');
-  //     cursor.foreach(function(doc, err) {
-  //       assert.strictEqual(null, err);
-  //       resultArray.push(doc);
-  //     }, 
-  //     function () {
-  //     db.close();
-  //     res.send({ resultArray })
-  //     })
-  //   })
-  // })
-  
-  // // INSERT data (insertOne)
-  // router.post('/insert', function(req, res, next) {
-  //   var item = {
-  //     title: req.body.title,
-  //     id: req.body.id,
-  //     thumbsUp: req.body.up,
-  //     thumbsDown: req.body.down
-  //   }
-  //   console.log(item)
-  //   mongo.connect(url, function(err, db) {
-  //     assert.strictEqual(null, err);
-  //     db.collection('MovieSearch').insertOne(item, function(err, result) {
-  //       assert.strictEqual(null, err);
-  //       console.log(result);
-  //       db.close();
-  //     })
-  //   })
-  // });
-  module.exports = router;
+module.exports = router;
